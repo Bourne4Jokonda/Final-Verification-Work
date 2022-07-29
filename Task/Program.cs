d06c7d1c93da98de15804a4ae300897d50c1f771
«Aleksandr» <bourne4jokonda@gmail.com>
2022-07-30 01:18:30 +0300

    change task decision

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,12 +7,13 @@
 // ["1234","1567","-2","computer science"]->["-2"]
 // ["Russia","Denmark","Kazan"]->[]
 
-Console.WriteLine("Заполните строку массива: ");
-string[] Texts1, Texts2, Texts3 ;
-string[] Result1, Result2, Result3 ;
-Texts1 = new string[4]{ "hello", "2", "world", ":-)"};
-Texts2 = new string[4]{"1234","1567","-2","computer science"};
-Texts3 = new string[3]{"Russia","Denmark","Kazan"};
-int t1=Texts1.Length;
-int t2=Texts2.Length;
-int t3=Texts3.Length;
+string[] texts=new string[4]{"hello","2","world",":-)"};
+int count = 0;
+for (int i = 0; i < texts.Length; i++)
+{
+      if (texts[i].Length <= 3)
+        {
+            count++;
+        }
+}
+string[] result=new string[count];
